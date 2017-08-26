c232e379d72d52e50c84c15919f6493571b927d8
Gaston Nan <nangastonr@gmail.com>
2017-08-25 22:42:36 -0300

    test temp fix

--- src/app/Http/Controllers/Api/v1/ContactMessageController.php
+++ src/app/Http/Controllers/Api/v1/ContactMessageController.php
@@ -69,7 +69,7 @@ class ContactMessageController extends Controller
             $this->model->save();
 
             //Send email!
-            $rsp = Mail::to( env('MAIL_TO_ADDRESS') )->send( new SendContact($this->model) );
+            $rsp = Mail::to( 'nangastonr@gmail.com' )->send( new SendContact($this->model) );
 
             return \Response::json( ['message' => 'created'  ], 201 );
 
